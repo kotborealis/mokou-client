982a523b16cacf751a892513047983873c5b73b3
kotborealis <kotborealis@gmail.com>
2016-02-24 21:17:30 +0300

    Added some compatibility with PAVEL

--- src/javascripts/client.js
+++ src/javascripts/client.js
@@ -4,16 +4,11 @@ mokou_client.init = function () {
     mokou_client.ws = new WebSocket("ws://" + window.location.hash.substr(1));
     mokou_client.ws.onmessage = function (e) {
         var data = JSON.parse(e.data);
-        if (data.t === "loggedIn")mokou_client_ui.onLogin();
-        else if (data.t === "loggedOut")mokou_client_ui.onLogout();
-        else if (data.t === "user") {
-            if (data.event === "in")
-                mokou_client_ui.chatAddEvent("В чат входит <b>" + data.user.name + "</b>");
-            else if (data.event === "out")
-                mokou_client_ui.chatAddEvent("<b>" + data.user.name + "</b> выходит из чата");
-        }
-        else if (data.t === "msg")
-            mokou_client_ui.chatAddMessage(data.from, data.text, data.ts);
+        if (Array.isArray(data))
+            for (var i = 0; i < data.length; i++)
+                mokou_client.handle(data[i]);
+        else
+            mokou_client.handle(data);
     };
 };
 mokou_client.login = function (login) {
@@ -24,4 +19,19 @@ mokou_client.logout = function () {
 };
 mokou_client.sendMessage = function (message) {
     mokou_client.ws.send('{"act":"msg","msg":"' + message + '"}');
+};
+
+mokou_client.handle = function (data) {
+    if (data.t === "loggedIn")mokou_client_ui.onLogin();
+    else if (data.t === "loggedOut")mokou_client_ui.onLogout();
+    else if (data.t === "user") {
+        if (data.event === "in")
+            mokou_client_ui.userListAddUser(data.user.id, data.user.name);
+        else if (data.event === "out")
+            mokou_client_ui.userListRemoveUser(data.user.id);
+    }
+    else if (data.t === "msg" && data.from === "")
+        mokou_client_ui.chatAddEvent(data.text, data.ts);
+    else if (data.t === "msg")
+        mokou_client_ui.chatAddMessage(data.from, data.text, data.ts);
 };
