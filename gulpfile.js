f7944e1aed0a87816fcfe6b60c12c4608c242ed2
kotborealis <kotborealis@gmail.com>
2016-03-02 09:49:33 +0300

    Added gulp-load to optimize gulp (lazy load)

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,36 @@
 var gulp = require("gulp");
-var uglify = require("gulp-uglify");
+var _ = require('gulp-load-plugins')();
+/*var uglify = require("gulp-uglify");
 var eslint = require("gulp-eslint");
 var less = require("gulp-less");
 var sourcemaps = require('gulp-sourcemaps');
 var babel = require("gulp-babel");
 var concat = require("gulp-concat");
-var replace = require("gulp-replace-task");
+var replace = require("gulp-replace-task");*/
 var path = require("path");
 
 gulp.task('css',function(){
 	return gulp.src(['./src/css/build_default.less'])
-		.pipe(sourcemaps.init())
-		.pipe(less())
-		.pipe(sourcemaps.write("./"))
+		.pipe(_.sourcemaps.init())
+		.pipe(_.less())
+		.pipe(_.sourcemaps.write("./"))
 		.pipe(gulp.dest('./public/css/'));
 });
 
 gulp.task('js',function(){
 	return gulp.src(['src/js/ui.js','src/js/ui-binginds.js','src/js/client.js','src/js/main.js','src/js/*.js'])
-        .pipe(sourcemaps.init())
-        .pipe(babel({
+        .pipe(_.sourcemaps.init())
+        .pipe(_.babel({
             presets: ['es2015']
         }))
-        .pipe(concat('build.js'))
-        .pipe(sourcemaps.write('.'))
+        .pipe(_.concat('build.js'))
+        .pipe(_.sourcemaps.write('.'))
         .pipe(gulp.dest('./public/js'))
 });
 
 gulp.task('html',function(){
 	return gulp.src(['src/*.html','src/manifest.json'])
-		.pipe(replace({
+		.pipe(_.replaceTask({
 			patterns: [
 	            {
 	                match: 'timestamp',
