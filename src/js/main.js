7fa3f062f096ddebe4f3560c20abc5bed5d7a19d
kotborealis <kotborealis@gmail.com>
2016-03-15 22:30:44 +0300

    Saving username to localStorage and restoring it on reload

--- src/js/main.js
+++ src/js/main.js
@@ -31,30 +31,25 @@ jQuery(function ($) {
      * Bind functions to UI elements
      */
     $('.users_button')[0].onclick = mokou.ui.userList.toggle();
-
     $("#loginForm")[0].onsubmit = function (e) {
         e.preventDefault();
         mokou.ui.act.login();
     };
-
     $("#messageForm")[0].onsubmit = function (e) {
         e.preventDefault();
         mokou.ui.act.message();
     };
-
     $("#chatExitButton")[0].onclick = function (e) {
         e.preventDefault();
         mokou.ui.act.logout();
     };
-
     $("#new_messages")[0].onclick = function (e) {
         e.preventDefault();
         mokou.ui.chat.scroll();
     };
-
     $('.chat').on('scroll', mokou.ui.chat.onscroll);
-
     $(window).on('resize', mokou.ui.chat.onscroll);
-
     window.onhashchange = mokou.client.init;
+
+    $("#loginInputNick").get(0).value = localStorage.getItem("username") || "";
 });
